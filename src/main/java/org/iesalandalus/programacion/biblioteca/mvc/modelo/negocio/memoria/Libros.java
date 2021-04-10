9afff6e5aeab3585eab190cc408aed4f6173a80c
lmr87 <lmr87@DESKTOP-G9P7QE6>
2021-04-10 12:51:21 +0200

    Punto 6. Clase Libros
    -Extraida la interfaz de clase Libros y colocada la clase y la interfaz
    en los paquetes adecuados
    -Cambiados algunos métodos de la clase Libros para que compruebe si el
    libro es instancia de LibroEscrito o de AudioLibro
    -Pasa todos los test

--- src/main/java/org/iesalandalus/programacion/biblioteca/mvc/modelo/negocio/memoria/Libros.java
+++ src/main/java/org/iesalandalus/programacion/biblioteca/mvc/modelo/negocio/memoria/Libros.java
@@ -1,4 +1,4 @@
-package org.iesalandalus.programacion.biblioteca.mvc.modelo.negocio;
+package org.iesalandalus.programacion.biblioteca.mvc.modelo.negocio.memoria;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -6,9 +6,12 @@ import java.util.List;
 
 import javax.naming.OperationNotSupportedException;
 
+import org.iesalandalus.programacion.biblioteca.mvc.modelo.dominio.AudioLibro;
 import org.iesalandalus.programacion.biblioteca.mvc.modelo.dominio.Libro;
+import org.iesalandalus.programacion.biblioteca.mvc.modelo.dominio.LibroEscrito;
+import org.iesalandalus.programacion.biblioteca.mvc.modelo.negocio.ILibros;
 
-public class Libros {
+public class Libros implements ILibros {
 
 	private List<Libro> coleccionLibros;
 
@@ -18,10 +21,11 @@ public class Libros {
 	}
 
 	/** Método para devolver una copia profunda de Libros */
+	@Override
 	public List<Libro> get() {
 		List<Libro> copiaLibros = copiaProfundaLibros();
 		copiaLibros.sort(Comparator.comparing(Libro::getTitulo).thenComparing(Libro::getAutor));
-		
+
 		return copiaLibros;
 	}
 
@@ -29,18 +33,28 @@ public class Libros {
 	private List<Libro> copiaProfundaLibros() {
 		List<Libro> copiaLibros = new ArrayList<>();
 
-		for (Libro libro : coleccionLibros)
-			copiaLibros.add(new Libro(libro));
+		// Para todos los libros de coleccionLibros
+		for (Libro libro : coleccionLibros) {
+			// Si es instancia de LibroEscrito
+			if (libro instanceof LibroEscrito)
+				copiaLibros.add(new LibroEscrito((LibroEscrito) libro));
+
+			// Si es instancia de AudioLibro
+			if (libro instanceof AudioLibro)
+				copiaLibros.add(new AudioLibro((AudioLibro) libro));
+		}
 
 		return copiaLibros;
 	}
 
 	/** Devuelve el tamaño de los libros ya insertados */
+	@Override
 	public int getTamano() {
 		return coleccionLibros.size();
 	}
 
 	/** Método para insertar libros nuevos */
+	@Override
 	public void insertar(Libro libro) throws OperationNotSupportedException {
 		if (libro == null)
 			throw new NullPointerException("ERROR: No se puede insertar un libro nulo.");
@@ -48,20 +62,31 @@ public class Libros {
 		if (coleccionLibros.contains(libro))
 			throw new OperationNotSupportedException("ERROR: Ya existe un libro con ese título y autor.");
 
-		coleccionLibros.add(new Libro(libro));
+		if (libro instanceof LibroEscrito)
+			coleccionLibros.add(new LibroEscrito((LibroEscrito) libro));
+
+		if (libro instanceof AudioLibro)
+			coleccionLibros.add(new AudioLibro((AudioLibro) libro));
 	}
 
 	/** Método para buscar un libro */
+	@Override
 	public Libro buscar(Libro libro) {
 		if (libro == null)
 			throw new IllegalArgumentException("ERROR: No se puede buscar un libro nulo.");
 
 		int indice = coleccionLibros.indexOf(libro);
 
-		return (indice == -1) ? null : new Libro(coleccionLibros.get(indice));
+		if (libro instanceof LibroEscrito)
+			return (indice == -1) ? null : new LibroEscrito((LibroEscrito) coleccionLibros.get(indice));
+
+		// Dejo este return fuera del if, ya que es igual que si pusiera else en el if
+		// anterior, si no es libro escrito, es audiolibro
+		return (indice == -1) ? null : new AudioLibro((AudioLibro) coleccionLibros.get(indice));
 	}
 
 	/** Método para borrar un libro. */
+	@Override
 	public void borrar(Libro libro) throws OperationNotSupportedException {
 		if (libro == null)
 			throw new IllegalArgumentException("ERROR: No se puede borrar un libro nulo.");
